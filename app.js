b6b7bf9cfd89718728ddb84914b1ab44201ac6e6
Farzad Towhidi <farzad.towhidi@gmail.com>
2012-10-04 18:43:38 -0400

    added css to it

--- app.js
+++ app.js
@@ -5,38 +5,65 @@ app.prototype =
 {
 	init:function()
 	{
-		var subscription = bayeux.subscribe("/test",function(message)
-					{
-						var canvas = document.getElementById("canvas");
-						var context = canvas.getContext('2d');
-						var points = JSON.parse(message.points);
-						
-						context.beginPath();
-						context.moveTo(points[0][0],points[0][1]);
-						for(var i=1;i<points.length;i++)
-						{
-							context.lineTo(points[i][0],points[i][1]);
-						}
-						context.stroke();
-					});
-		subscription.callback(function() {
-  					alert('Subscription is now active!');
-				});
-		subscription.errback(function(error) {
-  				alert(error.message);
-		});
-		
-		/*
-		var publication = bayeux.publish('/test', {text: 'TESTING'});
-	
-		publication.callback(function() {
-  			alert('Message received by server!');
+		var chatdiv = document.createElement('div');
+		chatdiv.id = 'chat-main';
+
+		var canvas = document.createElement("canvas");
+		canvas.id = 'canvas';
+
+		var replyboxctn = document.createElement('div');
+		replyboxctn.id = 'replybox-ctn'
+
+		var replybox = document.createElement('textarea');
+		replybox.id = 'replybox'
+
+		replyboxctn.appendChild(replybox);
+
+		document.body.appendChild(canvas);
+		document.body.appendChild(chatdiv);
+		document.body.appendChild(replyboxctn);
+
+
+		$(replybox).keypress(function(event){
+			switch(event.which || event.keyCode){
+				case 13:
+					bayeux.publish("/test",{chat:replybox.value});		
+				break;
+			}
 		});
 
-		publication.errback(function(error) {
-  			alert('There was a problem: ' + error.message);
+		var newChatMsg = function(msg){
+			var div=document.createElement('div');
+			div.className='chat-msg';
+			$(div).html(msg)
+		}
+
+		var subscription = bayeux.subscribe("/test",function(message){
+
+			if(message.chat){
+
+				return;
+			}
+
+			var context = canvas.getContext('2d');
+			var points = JSON.parse(message.points);
+			
+			context.beginPath();
+			context.moveTo(points[0][0],points[0][1]);
+			for(var i=1;i<points.length;i++)
+			{
+				context.lineTo(points[i][0],points[i][1]);
+			}
+			context.stroke();
 		});
-		*/
+
+		subscription.callback(function() {
+			alert('Welcome to DrawMe :)');
+		});
+		subscription.errback(function(error) {
+			alert('Something went wrong, refresh your browser.');
+		});
+		
 		new page();
 	}
 	
