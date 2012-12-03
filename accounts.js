bff1d2fcbcda6e0961f73b6cc0fbcb018bdd6ada
Farzad Towhidi <farzad.towhidi@gmail.com>
2012-12-02 20:43:10 -0500

    finished ui

--- accounts.js
+++ accounts.js
@@ -1,13 +1,183 @@
-function Account(data){
-
+function Account(data, menuBar){
+	this._menuBar = menuBar;
 	this.maindiv = document.createElement('div');
+	this.maindiv.className = "options-ctn";
 
-	$(this.maindiv).css({
-    	backgroundColor:'white',
-    	height:'200px',
-    	width:'200px'
-  	});
+	var returnButton = document.createElement('div');
+	returnButton.className = "return-button account button";
+	$(returnButton).html('<- Settings');
+	var self = this;
+	returnButton.onclick = function(){self._destroy();};
 
+	this.maindiv.appendChild(returnButton);
+
+
+	var createField = function(defaultText,isPassword){
+		var div = document.createElement('div');
+		div.className = 'input-box-wrapper blur';
+
+		var input = document.createElement('input');
+
+		div.appendChild(input)
+		
+		if(isPassword){
+			input.className = 'password'
+			input.type="password";
+			var wrappedDiv = document.createElement('div');
+			wrappedDiv.className = "blur password-wrapper";
+			$(wrappedDiv).html(defaultText);
+			$(div).prepend(wrappedDiv);
+
+			$(wrappedDiv).click(function(){onBlurFocusDiv(wrappedDiv,input,defaultText);});
+
+			$(input).blur(function(){
+				onBlurFocusDiv(wrappedDiv,input,defaultText,true);
+			});
+
+
+		}else{
+			input.value = defaultText;
+			input.type = "text";
+			input.className = 'blur';
+
+			$(input).blur(function(){onBlur(input,defaultText);});
+			$(input).focus(function(){onFocus(input,defaultText);});
+
+		}
+
+		div.setValue = function(text){
+			input.value = text;
+		}
+
+		div.getValue = function(){
+			return input.value
+		}
+
+
+		return div;
+	}
+
+	var onBlur = function(ctn,defaultText){
+		if(ctn.value === ""){
+			ctn.value = defaultText;
+			$(ctn).css('text-align','right');
+		}
+		$(ctn).addClass('blur');
+		$(ctn).removeClass('focus');
+	}
+
+	var onBlurFocusDiv  = function(ctn,wrappedDiv, defaultText, wrappedDivCaller){
+		if($(ctn).html() === "" && wrappedDiv.value === ""){
+			$(ctn).css('text-align','right');
+			$(ctn).html(defaultText);
+			$(ctn).addClass('blur');
+			$(ctn).removeClass('focus');
+		} else if($(ctn).html() === defaultText && wrappedDiv.value === ""){
+			$(ctn).html("");
+			$(ctn).removeClass('blur');
+			$(ctn).addClass('focus');
+			$(wrappedDiv).focus();
+		} else if(wrappedDiv.value !== "" && !wrappedDivCaller){
+			$(wrappedDiv).focus();
+			$(ctn).addClass('focus');
+		} else if(wrappedDivCaller){
+			$(ctn).removeClass('focus');
+			$(ctn).html("");
+		}
+	};
+
+	var onFocus = function(ctn,defaultText, text){
+		if(ctn.value === defaultText){
+			ctn.value = "";
+			$(ctn).css('text-align','left');
+		}	
+		$(ctn).removeClass('blur');
+		$(ctn).addClass('focus');
+	};
+
+	var nickname = createField('Nickname');
+	nickname.setValue(data.nickname);
+	var oldPassword = createField('Old Password',true);
+	var newPassword = createField('New Password',true);
+	var confirmPassword = createField('Confirm',true);
+
+	this.maindiv.appendChild(nickname);
+	this.maindiv.appendChild(oldPassword);
+	this.maindiv.appendChild(newPassword);
+	this.maindiv.appendChild(confirmPassword);
+
+	var validateNicknameForm = function(){
+		var a = nickname.getValue();
+		if(a==null || a ==''){
+			alert('Your nickname cannot be blank');
+			return false;
+		}
+		return true;
+	}
+
+	var validateAccountForm = function(){
+		var a = oldPassword.getValue();
+		var d = newPassword.getValue();
+		var e = confirmPassword.getValue();
+
+		if(a == null || a == ''){
+			alert('please enter your current password');
+			return false;
+		}
+		if(d==null || d=='' || e==null || e==''){
+			alert('Please enter new a password and confirm');
+			return false;
+		}
+
+		if(d!=e){
+			alert('Passwords do not match');
+			return false;
+		}
+	};
+
+
+	var saveButton = document.createElement('div');
+	saveButton.className = "stats button";
+	$(saveButton).html('Save');
+	$(saveButton).click(function(){
+		if(validateAccountForm){
+			var sendingData = {
+				nickname : nickname.getValue() || data.nickname()
+			}
+			
+			$.ajax({
+	      type: 'POST',
+	      url: '/changeNickname',
+	      data: sendingData,
+	      success: function(data){
+	      	alert('saved nickname');
+	      }
+	    });
+		}
+		if(validateNicknameForm()){
+
+			var sendingData = {
+				password : oldPassword.getValue(),
+				newPassword : newPassword.getValue(),
+				confirmPassword : confirmPassword.getValue()
+			}
+			
+
+			$.ajax({
+	      type: 'POST',
+	      url: '/changeAccount',
+	      data: sendingData,
+	      success: function(data){
+	      	alert('saved password');
+	      }
+	    });
+		}
+	});
+
+	this.maindiv.appendChild(saveButton);
+	
+
+/*
 	document.body.appendChild(this.maindiv);
 
 	var accountForm = document.createElement('form');
@@ -161,12 +331,13 @@ function Account(data){
 			return false;
 		}
 	};
-
+*/
 }
 
 Account.prototype = {
-	destroy: function(){
-		$(this.maindiv).hide();
+	_destroy: function(){
+		$(this.maindiv).remove();
+		this._menuBar.show();
 	},
 
 }
